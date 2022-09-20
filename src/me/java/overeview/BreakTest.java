dbcc6a527b971ed2677a162eb563f23710193699
lala9663 <lala96632040@gmail.com>
2022-09-20 17:39:35 +0900

    220920

---
+++ src/me/java/overeview/BreakTest.java
@@ -0,0 +1,22 @@
+package me.java.overeview;
+
+public class BreakTest {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num;
+
+//        for (num= 1; sum <= 100; num++) {
+//            sum += num;
+//        }
+//        System.out.println(sum);
+//        System.out.println(num);
+
+           for (num= 1; ; num++){
+               sum += num;
+               if( sum >= 100)
+                   break;
+           }
+            System.out.println(sum);
+            System.out.println(num);
+    }
+}
---
+++ src/me/java/overeview/ifpractice.java
@@ -0,0 +1,13 @@
+package me.java.overeview;
+
+public class ifpractice {
+    public static void main(String[] args) {
+    int seed = 10000000;
+        if (seed>=100000000 ){
+            System.out.println("100만원을 세금으로 낸다.");
+    }else {
+            System.out.println("세금을 내지 않는다.");
+    }
+        System.out.println("난 세금 안내야지");
+    }
+}
