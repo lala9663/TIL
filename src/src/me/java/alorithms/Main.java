b84d5050877dcdb62fc7cb15b2234df918ef4868
lala9663 <105348713+lala9663@users.noreply.github.com>
2022-10-03 17:40:10 +0900

    Delete src directory

--- src/src/me/java/alorithms/Main.java
+++
@@ -1,7 +0,0 @@
-package me.java.alorithms;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/src/me/java/alorithms/baekjoon/level1/Main.java
+++
@@ -1,4 +0,0 @@
-package me.java.alorithms.baekjoon.level1;
-
-public class Main {
-}
